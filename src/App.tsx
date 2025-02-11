7985b37e313491ee0fe658ee0b057714989a8aa8
Sbrendon98 <sylvestre.ba98@gmail.com>
2025-02-11 09:42:49 -0500

    Configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the App</h1>
+            <p>This is the front page</p>
+        </div>
+    )
+}
+
+export default App;
